92cda3a36a5611e5b503ffdbc69444c33ea9084f
Rhyvan <polyakan@gmail.com>
2020-06-09 13:47:30 +0200

    Fetch emails: connection, test

---
+++ SaintSender.Core.Tests/FetchEmailTests.cs
@@ -0,0 +1,18 @@
+﻿using EAGetMail;
+using NUnit.Framework;
+using SaintSender.Core.Services;
+
+namespace SaintSender.Core.Tests
+{
+    [TestFixture]
+    class FetchEmailTests
+    {
+        [Test]
+        public void Fetch_Mails_Are_Received()
+        {
+            var fetcher = new FetchEmail();
+            fetcher.FetchMail();
+            Assert.NotNull(fetcher.MailInfo);
+        }
+    }
+}
---
+++ SaintSender.Core/Services/FetchEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using EAGetMail;
+
+namespace SaintSender.Core.Services
+{
+    public class FetchEmail
+    {
+        public MailInfo[] MailInfo { get; set; }
+
+        public void FetchMail()
+        {
+            MailServer oServer = new MailServer("imap.gmail.com",
+                "cprogresssender@gmail.com",
+                "CPSpi1000101",
+                ServerProtocol.Imap4);
+            MailClient oClient = new MailClient("TryIt");
+            oServer.SSLConnection = true;
+            oServer.Port = 993;
+
+            try
+            {
+                oClient.Connect(oServer);
+                MailInfo = oClient.GetMailInfos();
+                oClient.Quit();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+    }
+}
