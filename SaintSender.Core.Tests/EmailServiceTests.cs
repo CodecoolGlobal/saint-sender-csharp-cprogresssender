af840f776df9bb4172da43b3510545a3e6a51f7f
Rhyvan <polyakan@gmail.com>
2020-06-09 22:44:51 +0200

    Add interface to EmalService, and modify corresponding test

---
+++ SaintSender.Core.Tests/EmailServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SaintSender.Core.Services;
+
+namespace SaintSender.Core.Tests
+{
+    [TestFixture]
+    class EmailServiceTests
+    {
+        [Test]
+        public void Fetch_Mails_Are_Received()
+        {
+            var fetcher = new EmailService();
+            var mails = fetcher.GetEmails();
+            Assert.NotNull(mails);
+        }
+    }
+}
---
+++ SaintSender.Core/Services/EmailService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using EAGetMail;
+using SaintSender.Core.Interfaces;
+
+namespace SaintSender.Core.Services
+{
+    public class EmailService: IEmailService
+    {
+        private List<Entities.Mail> _mails = new List<Entities.Mail>();
+        public MailInfo[] MailInfo { get; set; }
+
+        private void FetchMail()
+        {
+            MailServer oServer = new MailServer("imap.gmail.com",
+                "cprogresssender@gmail.com",
+                "CPSpi1000101",
+                ServerProtocol.Imap4);
+            MailClient oClient = new MailClient("TryIt");
+            oServer.SSLConnection = true;
+            oServer.Port = 993;
+
+            try
+            {
+                oClient.Connect(oServer);
+                ReceiveEmails(oClient);
+                oClient.Quit();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+
+        private void ReceiveEmails(MailClient client)
+        {
+            MailInfo[] mailInfos = client.GetMailInfos();
+            foreach (var mailInfo in mailInfos)
+            {
+                Mail oMail = client.GetMail(mailInfo);
+                _mails.Add(new Entities.Mail(
+                    oMail.From.ToString(),
+                    oMail.Subject.ToString(),
+                    oMail.TextBody,
+                    oMail.SentDate,
+                    mailInfo.Read
+                    ));
+            }
+
+        }
+
+        public List<Entities.Mail> GetEmails()
+        {
+            FetchMail();
+            return _mails;
+        }
+    }
+}
