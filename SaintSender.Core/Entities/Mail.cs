cd237cbacc1b5ed73e906b8ce6355f3ca81df149
Rhyvan <polyakan@gmail.com>
2020-06-11 23:57:54 +0200

    Bool to Fontweight, padding

--- SaintSender.Core/Entities/Mail.cs
+++ SaintSender.Core/Entities/Mail.cs
@@ -10,6 +10,15 @@ namespace SaintSender.Core.Entities
         public DateTime Date { get; set; }
         public bool Read { get; set; }
 
+        public string MessageWithoutCrlf
+        {
+            get
+            { 
+                var msg = Message.Replace("\r\n", " ").Trim();
+                return msg.Substring(0, Math.Min(msg.Length, 100)); 
+            }
+        }
+
         public Mail(string sender, string subject, string message, DateTime date, bool read)
         {
             Sender = sender;
